2b117a2724e0fbbac94092cd8996e383dd080833
WesleyVale99 <78268026+WesleyVale99@users.noreply.github.com>
2021-03-03 15:07:38 -0300

    Adicionar arquivos de projeto.

---
+++ ProcessKeyGet/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace ProcessTokenGet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(337, 166);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Seach";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(194, 190);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 2;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "-";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(113, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "About";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 217);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Process Token Build: 0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ProcessKeyGet/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessTokenGet
+{
+    public partial class Form1 : Form
+    {
+        public List<Process> processes = new List<Process>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            btnCopy.Enabled = false;
+            bool seach = false;
+
+            processes.Clear();
+            listBox1.Items.Clear();
+
+            Process[] processlist = Process.GetProcesses();
+            for (int i = 0; i < processlist.Length; i++)
+                processes.Add(processlist[i]);
+
+            for (int i = 0; i < processes.Count; i++)
+            {
+                label1.Text = i.ToString();
+                Process process = processes[i];
+                listBox1.Items.Add($"PID: {process.Id} Name: {process.ProcessName} SessionId: {process.SessionId}");
+                if (process.ProcessName == "PointBlank")
+                {
+                    MessageBox.Show("Point Blank found successfully.", "ProcessTokenGet", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    OnlyPb();
+                    seach = true;
+                    break;
+                }
+            }
+            if(!seach)
+               MessageBox.Show("Point Blank not found!", "ProcessTokenGet", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            label1.Text = "-";
+        }
+        private void OnlyPb()
+        {
+            for (int j = listBox1.Items.Count - 1; j > -1; j--)
+            {
+                bool seach = listBox1.Items[j].ToString().Contains("PointBlank");
+                if (!seach)
+                {
+                    listBox1.Items.RemoveAt(j);
+                    listBox1.Refresh();
+                }
+            }
+            for (int j = processes.Count - 1; j > -1; j--)
+            {
+                Process process = processes[j];
+                if (process.ProcessName != "PointBlank") //PointBlank
+                {
+                    processes.RemoveAt(j);
+                }
+            }
+            btnCopy.Enabled = true;
+        }
+        private static string GetCommandLine(int id)
+        {
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + id))
+            using (ManagementObjectCollection objects = searcher.Get())
+            {
+                return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString();
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < processes.Count; i++)
+            {
+                Process process = processes[i];
+                string[] copy = GetCommandLine(process.Id).Split('/');
+                if(copy != null)
+                {
+                    Clipboard.SetText(copy[2]);
+                    MessageBox.Show("Token Key Successfully copied.", "ProcessTokenGet", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Guide to help you quickly find the constant string \r\n (Token Key) \r\n " +
+                "By: Wesley Vale, Frank Lucas.", "ProcessKeyGet", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
